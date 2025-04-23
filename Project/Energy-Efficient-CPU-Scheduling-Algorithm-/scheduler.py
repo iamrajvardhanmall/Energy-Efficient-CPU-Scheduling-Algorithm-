51d4bacd1d3e684b5d03229115410c8f3855fa83
iamrajvardhanmall <rajvardhanmall@gmail.com>
2025-04-23 10:27:38 +0530

    Done

--- Project/Energy-Efficient-CPU-Scheduling-Algorithm-/scheduler.py
+++ Project/Energy-Efficient-CPU-Scheduling-Algorithm-/scheduler.py
@@ -3,9 +3,9 @@
 
 class Process:
     def __init__(self, pid, arrival_time, burst_time, priority):
-        self.pid = pid       
+        self.pid= pid       
         self.arrival_time = arrival_time
-        self.burst_time = burst_time
+        self.burst_ time = burst_time
         self.remaining_time = burst_time  # Tracks remaining execution time
         self.priority = priority
         self.start_time = None  # Will be set when process starts executing
